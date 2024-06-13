dde66584e47dc617bfffb047888a822f939cf349
Tejasvini-Wavge <teju.wavge11@gmail.com>
2024-06-13 10:55:57 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root=ReactDom.createRoot(document.getElementById("root"))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <>
+     <div class="container">
+        <div class="poster">
+            <a href="https://en.wikipedia.org/wiki/Laapataa_Ladies" target="_blank"><img src="https://tse2.mm.bing.net/th?id=OIP.wMNP5aqo0aFS2hOlcryyNgHaJQ&pid=Api&P=0&h=220" class="img"/></a>
+            <h2 class="name">Laapataa Ladies</h2>
+            
+        </div>
+        <div class="poster">
+            <a href="https://en.wikipedia.org/wiki/Yodha_(2024_film)"><img src="https://tse3.mm.bing.net/th?id=OIP.EURv-D17MQaWZ1WcBz5czgHaJZ&pid=Api&P=0&h=220" class="img"/></a>
+            <h2 class="name">Yodha</h2>
+        </div>
+        <div class="poster">
+            <a href="https://en.wikipedia.org/wiki/Sita_Ramam"><img src="https://tse3.mm.bing.net/th?id=OIP.R5z4qFjTFCDzmrxvbwaI6AHaKX&pid=Api&P=0&h=220" class="img"/></a>
+            <h2 class="name">Sita Raman</h2>
+        </div>
+    </div>
+    </>
+)
